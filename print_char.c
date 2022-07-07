3aa23bcefa160fc79e6941d83a98aed1124fcac8
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-07 10:59:26 +0000

    Description added

--- print_char.c
+++ print_char.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * _print_c - Prints characters
+ * print_char - Prints characters
  * @lst: is the list
+ *
+ * Return: (1)
  */
 
 int print_char(va_list lst)
 {
 	char c = va_arg(lst, int);
+
 	write(STDOUT_FILENO, &c, 1);
 
 	return (1);
